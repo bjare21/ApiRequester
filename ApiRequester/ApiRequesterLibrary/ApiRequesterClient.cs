93be9c8c3874c98d4d44bb654087dcd218817c0f
bjare21 <jarek.barecki@gmail.com>
2023-08-09 18:04:49 +0200

    PostAsync
    PostAsync method created

--- ApiRequester/ApiRequesterLibrary/ApiRequesterClient.cs
+++ ApiRequester/ApiRequesterLibrary/ApiRequesterClient.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
 namespace ApiRequesterLibrary;
 public class ApiRequesterClient
@@ -12,8 +13,10 @@ public class ApiRequesterClient
         this.serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
     }
 
-    public async Task<IReadOnlyCollection<T>> GetListAsync<T>(string uri, CancellationToken cancellationToken = default)
+    public async Task<ResponseData<IReadOnlyCollection<T>>> GetListAsync<T>(string uri, CancellationToken cancellationToken = default)
     {
+        var responseData = new ResponseData<IReadOnlyCollection<T>>();
+
         var request = new HttpRequestMessage(HttpMethod.Get, uri);
         using (var result = await this.httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
         {
@@ -22,10 +25,36 @@ public class ApiRequesterClient
                 using (var streamReader = new StreamReader(responseStream))
                 using (var jsonTextReader = new JsonTextReader(streamReader))
                 {
-                    var res = this.serializer.Deserialize<ResponseData<List<T>>>(jsonTextReader);
-                    return res.Data;
+                    responseData = this.serializer
+                        .Deserialize<ResponseData<IReadOnlyCollection<T>>>(jsonTextReader);
                 }
             }
         }
+
+        return responseData;
+    }
+
+    public async Task<ResponseData<T>> PostAsync<T>(T item, string uri, CancellationToken cancellationToken)
+    {
+        var responseData = new ResponseData<T>();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, uri);
+
+        var jsonData = JsonConvert.SerializeObject(item);
+        var stringContent = new StringContent(jsonData, UnicodeEncoding.UTF8, "application/json");
+
+        using (var response = await this.httpClient.PostAsync(uri, stringContent, cancellationToken))
+        {
+            using (var responsStream = await response.Content.ReadAsStreamAsync(cancellationToken))
+            {
+                using (var streamReader = new StreamReader(responsStream))
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    responseData = this.serializer.Deserialize<ResponseData<T>>(jsonTextReader);
+                }
+            }
+        }
+
+        return responseData;
     }
 }
